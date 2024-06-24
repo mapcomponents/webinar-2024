a31782d332d64c95e750d52dde2498f27cac0528
Martin Alzueta <martin.alzueta@wheregroup.com>
2024-06-24 13:05:31 +0200

    step 6 to 7

---
+++ Step6/src/Layers/OSMLayer.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import {
+  MlGeoJsonLayer,
+  useAddProtocol,
+  OSMProtocolHandler,
+  useAddImage,  
+} from "@mapcomponents/react-maplibre";
+
+import { Button } from "@mui/material";
+
+const osmOptions = {
+  completeFeature: true,
+  renderTagged: false,
+  excludeWay: true
+};
+const optionsURL = "?" +  encodeURI(JSON.stringify(osmOptions));
+
+
+export default function OSMLayer() {
+
+  const [showRettungspunkte, setShowRettungspunkte] = useState(false);
+  const buttonColor = showRettungspunkte ? "grey" : "green";
+
+  useAddProtocol({
+    protocol: "osm",
+    handler: OSMProtocolHandler,
+  });
+
+  useAddImage({
+    imageId: "rettungs-punkt",
+    imagePath: "/rettungspunkt.jpg"
+  })
+
+
+  return (
+    <>
+      {showRettungspunkte && 
+        <MlGeoJsonLayer
+          layerId={"osm-rettung"}
+          type="symbol"
+          options={{          
+            source: {
+                type: "geojson",
+                data: "osm://sources/schauinsland.osm" + optionsURL,
+              },
+            paint: { "icon-opacity": 0.8 },
+            layout: {
+              "icon-image": "rettungs-punkt",
+              "icon-size": 0.07,
+              "icon-anchor": "bottom",             
+              "icon-allow-overlap": true,
+            },
+          }}
+          labelProp="ref"
+          onClick={(event) =>
+            alert(
+              "Id: " +
+                JSON.stringify(event.features[0].properties["id"]) +
+                "\nOperator: " +
+                JSON.stringify(event.features[0].properties["operator"])
+            )
+          }
+        />
+      }
+
+      <Button
+        variant="contained"
+        onClick={() => setShowRettungspunkte(!showRettungspunkte)}
+        sx={{ left: "90%", backgroundColor: showRettungspunkte ? "grey" : "green" }}
+      >
+        Rettungspunkte
+      </Button>
+    </>
+  );
+}
---
+++ Step6/src/Layers/ProjectLayers.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import {
+  LayerList,
+  LayerListItem,
+  MlGeoJsonLayer,
+  Sidebar,
+  useAddProtocol,
+  CSVProtocolHandler,
+  AddLayerButton,
+  useSource
+} from "@mapcomponents/react-maplibre";
+import monitoringAreas from "./monitoringAreas.json";
+import {Button} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export default function ProjectLayers() {
+
+  useAddProtocol({
+    protocol: "csv",
+    handler: CSVProtocolHandler,
+  });
+
+  
+  return (
+    <>
+      <Sidebar open={true} name={"Layers"}>
+              
+        <LayerList>
+          <LayerListItem
+            visible={true}
+            configurable={true}
+            type="layer"
+            layerId="monitoring"
+            name="Monitoring areas"
+            layerComponent={
+              <MlGeoJsonLayer
+                geojson={monitoringAreas}
+                options={{
+                  paint: {
+                    "fill-color": "#5353ec",
+                    "fill-opacity": 0.5,
+                    "fill-outline-color": "#000",
+                  },
+                }}
+              />
+            }
+          />
+          <LayerListItem
+            name="Samples"
+            configurable={true}
+            layerComponent={
+              <MlGeoJsonLayer
+                layerId="samples"
+                type="circle"
+                options={{
+                  source:{
+                    type: "geojson",
+                    data: "csv://sources/samples.csv",
+                  },
+                  paint: {
+                    "circle-color": "#22BB5D",
+                    "circle-stroke-width": 0,
+                  },
+                }}
+                labelProp="id"
+                labelOptions={{                  
+                  layout: {
+                    "text-size": {
+                      stops: [
+                        [13, 15],
+                        [22, 60],
+                      ],
+                    },
+                  },
+                  minzoom: 13,
+                }}
+              />
+            }
+          />
+         
+        </LayerList>
+      </Sidebar>
+    </>
+  );
+}
+
+/*
+ */
